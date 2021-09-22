bf2681a40ae0b954fad4234f36189b319fab041b
Chi-Claudette <chiclaudette2000@gmail.com>
2021-09-22 15:51:56 +0100

    created the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LandingComponent } from './landing/landing.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ManageCategoryComponent } from './manage-category/manage-category.component';
+import { EditCatComponent } from './manage-category/edit-cat/edit-cat.component';
+import { CreateCatComponent } from './create-cat/create-cat.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { ManageProductComponent } from './manage-product/manage-product.component';
+import { EditProductComponent } from './manage-product/edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LandingComponent,
+    SignupComponent,
+    LoginComponent,
+    ManageCategoryComponent,
+    EditCatComponent,
+    CreateCatComponent,
+    CreateProductComponent,
+    ManageProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
